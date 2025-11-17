43c37a75e93439e739474b13cbb276e831bab3af
Dominik Stanojevic <dominik.stanojevic@outlook.com>
2025-11-17 15:35:02 +0800

    Fixing libtorch issue

---
+++ build.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let os = std::env::var("CARGO_CFG_TARGET_OS").expect("Unable to get TARGET_OS");
+    match os.as_str() {
+        "linux" | "windows" => {
+            if let Some(lib_path) = std::env::var_os("DEP_TCH_LIBTORCH_LIB") {
+                println!(
+                    "cargo:rustc-link-arg=-Wl,-rpath={}",
+                    lib_path.to_string_lossy()
+                );
+            }
+            println!("cargo:rustc-link-arg=-Wl,--no-as-needed");
+            println!("cargo:rustc-link-arg=-ltorch");
+        }
+        _ => {}
+    }
+}
